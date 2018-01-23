5a214c0fc1b3e1ad175aa5d20a8c9df7b79c2d10
ilya <ilya>
2018-01-23 16:54:59 +0200

    i renewed desing

---
+++ js.js
@@ -0,0 +1,7 @@
+var chek = 0;
+for (var chek = 0; chek < 9999; i++) {
+   chek++;
+   var table = document.createElement('chek');
+    var tr = document.createElement('chek');
+    tr.innerHTML = ".";
+}
