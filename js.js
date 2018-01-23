966498f54291df46a1b382f6457f70c76e8189ac
ilya <ilya>
2018-01-23 17:49:24 +0200

    renewed reting

--- js.js
+++
@@ -1,7 +0,0 @@
-var chek = 0;
-for (var chek = 0; chek < 9999; i++) {
-   chek++;
-   var table = document.createElement('chek');
-    var tr = document.createElement('chek');
-    tr.innerHTML = ".";
-}
